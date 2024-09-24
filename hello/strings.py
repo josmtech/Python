81bde89bd202484f580a6d883eb280216c1ff1f0
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2024-09-24 18:08:09 +0300

    strings and string functions

---
+++ hello/strings.py
@@ -0,0 +1,8 @@
+course = 'python for beginners'
+
+print(course.upper())
+print(course.find('y'))
+print(course.replace('for', '4'))
+print(course.find('python'))
+print('python' in course)
+print(course)
