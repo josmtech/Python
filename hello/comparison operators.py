0c81acce162035cfe3ee2760a6de6d638adb3599
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2024-09-24 18:27:13 +0300

    more practice on comparison operators

---
+++ hello/comparison operators.py
@@ -0,0 +1,23 @@
+#greater than operator
+x = 3 > 2
+print(x)
+
+#greater than or equal to operator
+n = 3 >= 2
+print(n)
+
+#less than operator
+y = 3 < 2
+print(y)
+
+#greater than or equal to operator
+o = 3 <= 2
+print(o)
+
+#Equal to
+w = 3 == 2
+print(w)
+
+#not equal to
+z = 3 != 2
+print(z)
