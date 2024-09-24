cf33744babe9e8ddc627a3bbcfd5ead6cf677515
Joseph Mumo Mbetu <josephmmbetu@gmail.com>
2024-09-24 17:33:57 +0300

    added first excercise on variables

---
+++ hello/excercise1.py
@@ -0,0 +1,10 @@
+#checking in a new patient into the system
+
+#declaring variables
+patient_first_name = "John"
+patient_last_name = "Smith"
+age = 20
+visit = "first"
+
+#printing output
+print("Today we received patient named " +patient_first_name + " " +patient_last_name +". He is " +str(age) +" years old and visiting for the " +visit +" time. Welcome" )
